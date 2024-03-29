1c592b6c736bab5b7f9821e0b5cd08ca40f477a6
Satrio Wirayuda <oppof5yuda@gmail.com>
2024-03-29 16:23:11 +0700

    Memberi pilihan kepada pengguna

--- app/src/main/java/org/d3if0107/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0107/asesment1/ui/screen/MainScreen.kt
@@ -1,15 +1,22 @@
 package org.d3if0107.asesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,8 +26,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
@@ -53,11 +63,20 @@ fun ScreenContent(modifier: Modifier) {
     var nomer_customer by remember { mutableStateOf("") }
     var jumlah_sepatu by remember { mutableStateOf("") }
 
+    val radioOptions = listOf(
+        stringResource(id = R.string.hari_1),
+        stringResource(id = R.string.hari_2),
+        stringResource(id = R.string.hari_3),
+    )
+    var day by remember { mutableStateOf(radioOptions[0]) }
+
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = stringResource(id = R.string.laundry_intro),
@@ -91,13 +110,48 @@ fun ScreenContent(modifier: Modifier) {
             value = jumlah_sepatu,
             onValueChange = { jumlah_sepatu = it },
             label = { Text(text = stringResource(id = R.string.jumlah_sepatu)) },
-            trailingIcon = { Text(text = "pcs")},
+            trailingIcon = { Text(text = "pcs") },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
             ),
             modifier = Modifier.fillMaxWidth()
+        )
+        Row (
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ){
+            radioOptions.forEach { text ->
+                DayOption(
+                    label = text,
+                    isSelected = day == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = day == text,
+                            onClick = { day = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DayOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
         )
     }
 }
