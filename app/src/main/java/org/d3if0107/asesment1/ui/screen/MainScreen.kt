9b8d9be5b68c77b6869e282980dd276249130bc1
Satrio Wirayuda <oppof5yuda@gmail.com>
2024-03-29 15:43:56 +0700

    Meminta input data dari pengguna

--- app/src/main/java/org/d3if0107/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0107/asesment1/ui/screen/MainScreen.kt
@@ -1,18 +1,29 @@
 package org.d3if0107.asesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if0107.asesment1.R
@@ -37,13 +48,57 @@ fun MainScreen() {
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier){
-    Column (
+fun ScreenContent(modifier: Modifier) {
+    var nama_customer by remember { mutableStateOf("") }
+    var nomer_customer by remember { mutableStateOf("") }
+    var jumlah_sepatu by remember { mutableStateOf("") }
+
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
-    ){
-
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.laundry_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = nama_customer,
+            onValueChange = { nama_customer = it },
+            label = { Text(text = stringResource(id = R.string.nama_customer)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = nomer_customer,
+            onValueChange = { nomer_customer = it },
+            label = { Text(text = stringResource(id = R.string.nomer_customer)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = jumlah_sepatu,
+            onValueChange = { jumlah_sepatu = it },
+            label = { Text(text = stringResource(id = R.string.jumlah_sepatu)) },
+            trailingIcon = { Text(text = "pcs")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
