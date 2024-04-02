76cccf03aa97067fedc4a39b0009f5124b6142a5
Satrio Wirayuda <oppof5yuda@gmail.com>
2024-04-02 15:06:33 +0700

    Membuat tampilan about

--- app/src/main/java/org/d3if0107/asesment1/navigation/NavGraph.kt
+++ app/src/main/java/org/d3if0107/asesment1/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import org.d3if0107.asesment1.ui.screen.AboutScreen
 import org.d3if0107.asesment1.ui.screen.MainScreen
 
 @Composable
@@ -15,6 +16,9 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()){
     ){
         composable(route = Screen.Home.route){
             MainScreen()
+        }
+        composable(route = Screen.About.route){
+            AboutScreen()
         }
     }
 }
--- app/src/main/java/org/d3if0107/asesment1/navigation/Screen.kt
+++ app/src/main/java/org/d3if0107/asesment1/navigation/Screen.kt
@@ -3,4 +3,5 @@ package org.d3if0107.asesment1.navigation
 
 sealed class Screen (val route: String){
     data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
